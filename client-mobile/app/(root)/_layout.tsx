ecfc993231382b51406200c47aaf6bb111c7986b
Jeffy Evangelistam <ejeffydev@gmail.com>
2025-07-10 10:33:14 +0800

    feat: clerk authentication setup

---
+++ client-mobile/app/(root)/_layout.tsx
@@ -0,0 +1,13 @@
+import { useUser } from "@clerk/clerk-expo";
+import { Redirect } from "expo-router";
+import { Stack } from "expo-router/stack";
+
+export default function Layout() {
+  const { isSignedIn } = useUser();
+
+  if (!isSignedIn) {
+    return <Redirect href={"/(auth)/sign-in"} />;
+  }
+
+  return <Stack />;
+}
